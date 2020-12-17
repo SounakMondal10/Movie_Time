dcca4521f61d43393c11e07b9783e15661ab1dde
Sounak Mondal <sounakmondal10@gmail.com>
2020-12-17 11:40:42 +0530

    Designing Layouts

---
+++ app/src/main/java/com/sounakmondal/movietime/Adaptery.java
@@ -0,0 +1,64 @@
+package com.sounakmondal.movietime;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+import java.util.zip.Inflater;
+
+public class Adaptery extends RecyclerView.Adapter<Adaptery.myViewHolder> {
+
+    private Context mContext;
+    private List<MovieModelClass> mData;
+
+    public Adaptery(Context mContext, List<MovieModelClass> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
+    }
+
+    @NonNull
+    @Override
+    public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View v = inflater.inflate(R.layout.movie_item, parent,false);
+        return new myViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull myViewHolder holder, int position) {
+        holder.name.setText(mData.get(position).getName());
+        holder.id.setText(mData.get(position).getId());
+        Glide.with(mContext)
+                .load(mData.get(position).getImg())
+                .into(holder.img);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public static class myViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView name, id;
+        ImageView img;
+
+        public myViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.name_txt);
+            id = itemView.findViewById(R.id.id_txt);
+            img = itemView.findViewById(R.id.imageView);
+        }
+    }
+}
