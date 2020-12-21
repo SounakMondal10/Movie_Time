86175b3cc7505f29f4e6b6f71309e4f814212e5d
Sounak Mondal <sounakmondal10@gmail.com>
2020-12-21 19:54:14 +0530

    -

--- app/src/main/java/com/sounakmondal/movietime/MainActivity.java
+++ app/src/main/java/com/sounakmondal/movietime/MainActivity.java
@@ -1,16 +1,24 @@
 package com.sounakmondal.movietime;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -39,22 +47,39 @@ public class MainActivity extends AppCompatActivity {
     //API ReadAccessToken (V4) - eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYTM2NWExNWRmMDNhNTdhYTJjOWU5YzU0N2MzYmJjMyIsInN1YiI6IjVmZGFkOTc3ODU4Njc4MDAzZmY1YzhmNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j7cm0KcsKod8xSNpDirzcY3h9EYOGe1EN-TjoRWD47U
     //For images, Add before path - https://image.tmdb.org/t/p/original/
 
-    public static String JSON_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=0a365a15df03a57aa2c9e9c547c3bbc3&language=en-US&page="; //add "1" in the end to revert
+    public static String JSON_URL; //add "1" in the end to revert
+    public static String JSON_URL_Movies = "https://api.themoviedb.org/3/movie/now_playing?api_key=0a365a15df03a57aa2c9e9c547c3bbc3&language=en-US&page=";
+    public static String JSON_URL_TVSeries = "https://api.themoviedb.org/3/tv/popular?api_key=0a365a15df03a57aa2c9e9c547c3bbc3&language=en-US&page=";
     List<MovieModelClass> movieList;
     RecyclerView recyclerView;
     Button loadMoreButton;
     public int lastMovieSeenPosition = 0;
     boolean mHasReachedBottom = false;
     Snackbar loadingSnackbar;
+    Toolbar main_toolbar;
+    Spinner main_spinner;
+    GetData getData;
+    Boolean isMovie;
+
 
     public static int currentPage = 1;
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        JSON_URL = JSON_URL_Movies;
+        isMovie = true;
 
+        main_toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(main_toolbar);
+        main_spinner = findViewById(R.id.mainActivity_spinner);
+        ArrayList<String> watchOptions = new ArrayList<String>();
+        watchOptions.add("Movies");
+        watchOptions.add("TV Series");
 
-
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1, watchOptions);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        main_spinner.setAdapter(spinnerAdapter);
 
 
         movieList = new ArrayList<>();
@@ -63,8 +88,53 @@ public class MainActivity extends AppCompatActivity {
 
         JSON_URL = JSON_URL + currentPage;
 
-        GetData getData = new GetData();
+        getData = new GetData();
         getData.execute();
+
+
+        main_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                Toast.makeText(MainActivity.this, Integer.toString(position), Toast.LENGTH_SHORT).show();
+//                0 for movies, 1 for TV Series
+                if(position == 1)
+                {
+                    getData.cancel(true);
+                    JSON_URL = JSON_URL_TVSeries;
+                    currentPage = 1;
+                    JSON_URL+=currentPage;
+                    movieList = new ArrayList<>();
+                    loadingSnackbar = Snackbar.make(recyclerView,"Loading TV Series", Snackbar.LENGTH_LONG);
+                    loadingSnackbar.show();
+                    getData = new GetData();
+                    getData.execute();
+                    isMovie = false;
+
+                }
+                else
+                {
+                    getData.cancel(true);
+                    JSON_URL = JSON_URL_Movies;
+                    currentPage = 1;
+                    JSON_URL+=currentPage;
+                    movieList = new ArrayList<>();
+                    if(isMovie == false)
+                    {
+                        loadingSnackbar = Snackbar.make(recyclerView,"Loading Movies", Snackbar.LENGTH_LONG);
+                        loadingSnackbar.show();
+                    }
+                    getData = new GetData();
+                    getData.execute();
+                    isMovie = true;
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
 
         //Auto-refresh data when at the end of the list
@@ -75,10 +145,11 @@ public class MainActivity extends AppCompatActivity {
 
                 if (!recyclerView.canScrollVertically(1) && !mHasReachedBottom) {
 
+                    currentPage+=1;
+
                     loadingSnackbar = Snackbar.make(recyclerView,"Loading More", Snackbar.LENGTH_INDEFINITE);
                     loadingSnackbar.show();
 
-                    currentPage+=1;
                     if(currentPage<=10)
                     {
                         JSON_URL = JSON_URL.substring(0, JSON_URL.length() - 1);
@@ -157,8 +228,15 @@ public class MainActivity extends AppCompatActivity {
                     JSONObject jsonObject1 = jsonArray.getJSONObject(i);
                     //Log.i("JSON object id", jsonObject1.getString("id"));
                     MovieModelClass model = new MovieModelClass();
+                    if(isMovie == true)
+                    {
+                        model.setName(jsonObject1.getString("original_title"));
+                    }
+                    else
+                    {
+                        model.setName(jsonObject1.getString("original_name"));
+                    }
                     model.setId(jsonObject1.getString("vote_average"));
-                    model.setName(jsonObject1.getString("original_title"));
                     model.setImg("https://image.tmdb.org/t/p/original/" + jsonObject1.getString("poster_path"));
                     model.setDescription(jsonObject1.getString("overview"));
                     movieList.add(model);
@@ -175,7 +253,11 @@ public class MainActivity extends AppCompatActivity {
             recyclerView.scrollToPosition(lastMovieSeenPosition -7);
 
                 try{
-                    loadingSnackbar.dismiss();
+                    if(currentPage!=1)
+                    {
+                        loadingSnackbar.dismiss();
+                    }
+
                 }catch (Exception e)
                 {
                     e.printStackTrace();
@@ -191,7 +273,6 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
         recyclerView.setAdapter(adaptery);
     }
-
 
 
 
